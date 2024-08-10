57e6bd4343a0e199a592e9fe61b485a96816be24
Yoosuf Aathil <yoosufaath007@gmail.com>
2024-08-10 12:47:58 +0530

    add static file serving for public folder

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ mongoose.connect("mongodb://localhost/CRMdb", {});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//serving static files
+app.use(express.static("public"));
+
 routes(app);
 
 app.get("/", (req, res) => {
